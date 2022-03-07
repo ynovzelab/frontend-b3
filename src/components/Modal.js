6576e6e9c0f3cf96f5f535a6cc0bd8edcf52419e
Vincent Zerbib <contact@zelabstudio.com>
2022-03-07 15:43:37 +0100

    save worjk

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,17 +3,39 @@ import React from "react";
 const Modal = (props) => {
   return (
     <>
-      <div className="overlay"></div>
-      <div className="modal">
-        <div className="modal__title">
-          <h2>{props.title}</h2>
-        </div>
-        <div className="modal__content">
-            {props.children}
-        </div>
-        <div className="modal__actions">    
-        </div>
-      </div>
+      {props.isActive ? (
+        <>
+          <div className="overlay" onClick={props.closefunction}></div>
+          <div className="modal">
+            <div className="modal__header">
+              <div className="close__cross" onClick={props.closefunction}>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div className="modal__title">
+              <h2>{props.title}</h2>
+            </div>
+            <div className="modal__content">{props.children}</div>
+            <div className="modal__actions">
+              <button
+                className="btn btn__color-white"
+                onClick={props.closefunction}
+              >
+                Annuler
+              </button>
+              <button
+                className="btn btn__color-black"
+                onClick={props.validateFunction}
+              >
+                Valider
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 };
